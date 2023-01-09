92d5b79ed6c2b40e69e5ecedbaec89368ec69eff
rizkifa4 <rizkifachrozy@gmail.com>
2023-01-09 13:01:52 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/dicoding_wisata_bandung/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dicoding_wisata_bandung
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/favorite_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class FavoriteButton extends StatefulWidget {
+  const FavoriteButton({super.key});
+
+  @override
+  State<FavoriteButton> createState() => _FavoriteButtonState();
+}
+
+class _FavoriteButtonState extends State<FavoriteButton> {
+  bool isFavorited = false;
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        isFavorited ? Icons.favorite : Icons.favorite_border,
+        color: Colors.red,
+      ),
+      onPressed: () {
+        setState(() {
+          isFavorited = !isFavorited;
+        });
+      },
+    );
+  }
+}
